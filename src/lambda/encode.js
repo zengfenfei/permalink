6789ba92f588b27b7410a442ac5664f7fad9dbb5
Kevin Zeng <zengfenfei@gmail.com>
2018-03-13 00:20:08 +0800

    Encode original RC url to permalink

--- src/lambda/encode.js
+++ src/lambda/encode.js
@@ -1,10 +1,13 @@
+import * as crypto from 'crypto'
 
 export function get(event, context, callback) {
+    let cryptKey = Buffer.from(process.env['urlCryptKey'])
+    let rcUrl = decodeURIComponent(event.pathParameters['rcUrl']);
+    let cipher = crypto.createCipher('AES-256-ECB', cryptKey)
+    let encrypted = cipher.update(Buffer.from(rcUrl))
+    encrypted = Buffer.concat([encrypted, cipher.final()])
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify({
-            event,
-            context
-        })
+        body: JSON.stringify({ permaLink: encrypted.toString('base64'), error: null, message: 'ok' })
     })
 }
