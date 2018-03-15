564e81afe90764047533ceb38f7798a5a6030f1e
Kevin Zeng <zengfenfei@gmail.com>
2018-03-15 14:42:47 +0800

    Handle oauth callback errors

--- src/lambda/oauth-callback.js
+++ src/lambda/oauth-callback.js
@@ -2,6 +2,7 @@ import * as cookie from 'cookie'
 import { encrypt } from "../crypto";
 import { getTokenByCode } from "../rc";
 import config from '../config'
+import { stat } from 'fs';
 
 export async function get(event, context, callback) {
     let { queryStringParameters } = event
@@ -13,25 +14,35 @@ export async function get(event, context, callback) {
         return
     }
     let { code, state, error, error_description } = queryStringParameters
-    if (code && state) {
-        state = state.replace(/\s/g, '+')   // FIXME, a bug of oauth, the '+' in 'state' will become space when they're passed back
-        let token = await getTokenByCode(code)
-        callback(null, {
-            statusCode: 302,
-            headers: {
-                'Set-Cookie': cookie.serialize(
-                    'rc-token',
-                    encrypt(token.accessToken),
-                    { path: '/', expires: new Date(token.expiresIn), httpOnly: true }
-                ),
-                Location: config.proxyBaseUrl + encodeURIComponent(state)
-            }
-        })
-    } else {
+    if (!code || !state) {
         callback(null, {
             statusCode: 403,
             body: `Login failed. Error: ${error}, ${error_description}. You can login again and authorize this app.`
         })
+        return
     }
+    state = state.replace(/\s/g, '+')   // FIXME, a bug of oauth, the '+' in 'state' will become space when they're passed back
+    let token
+    try {
+        token = await getTokenByCode(code)
+    } catch (e) {
+        callback(null, {
+            statusCode: 403,
+            body: "Please check your RC credentials. " + e
+        })
+        return
+    }
+
+    callback(null, {
+        statusCode: 302,
+        headers: {
+            'Set-Cookie': cookie.serialize(
+                'rc-token',
+                encrypt(token.accessToken),
+                { path: '/', expires: new Date(token.expiresIn), httpOnly: true }
+            ),
+            Location: config.proxyBaseUrl + encodeURIComponent(state)
+        }
+    })
 
 }
