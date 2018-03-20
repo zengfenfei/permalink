61e12d26cc972712c255934dfe5f21044bf51ee8
Kevin Zeng <zengfenfei@gmail.com>
2018-03-20 11:24:50 +0800

    Support dev config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,11 @@
-// Set up this file, so that others files using config don't need to care about the location of config file
-export default require('../data/config.json')
+// Set up this file, so that others files using config don't need to care about the physical location of config file
+import * as assign from "assign-deep";
+
+let config = require('../data/config.json')
+
+// You should call this function only once
+export function setStage(stage) {
+    assign(config, require('../data/config.' + stage + '.json'))
+}
+
+export default config
