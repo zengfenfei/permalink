7e91fa4b818e292992304ffa0b2e4ee115b784b4
Kevin Zeng <zengfenfei@gmail.com>
2018-03-20 15:10:39 +0800

    Encode supports sandbox and production

--- src/lambda/encode.js
+++ src/lambda/encode.js
@@ -1,10 +1,23 @@
+import * as url from 'url'
 import { encrypt } from '../crypto'
-import config from '../config'
+import config, { setStage } from '../config'
 
 export function get(event, context, callback) {
     let rcUrl = decodeURIComponent(event.pathParameters['rcUrl']);
-    let permaLink = config.proxyBaseUrl + encodeURIComponent(encrypt(rcUrl))
+    rcUrl = url.parse(rcUrl)
+    let { hostname } = rcUrl
+    if (!hostname) {
+        callback(null, {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Bad url' })
+        })
+        return
+    }
+    if (hostname.match(/\bdevtest\b/)) {
+        setStage('dev')
 
+    }
+    let permaLink = config.proxyBaseUrl + encodeURIComponent(encrypt(rcUrl.path))
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({ permaLink, error: null, message: 'ok' })
